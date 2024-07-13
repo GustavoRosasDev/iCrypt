f28fe2fa367b2fdfc4436b68b4b507be52020eee
GustavoRosas-Dev <python.dev.br@gmail.com>
2024-07-13 17:35:05 -0300

    ⚡ General update: Added tabs ('Home' and 'Info'), copy content (green button) and functionality to clear fields ('Enter the key here' and 'text_box').

--- main.py
+++ main.py
@@ -22,17 +22,57 @@ def main():
     """
 
     global slider, slider_value, entry_key, text_box, start_button
+
+    # region 🟢 Tabs
+
+    # region 🟩 Initial Setup (margins and borders)
+
+    # Creation of the 'tab' Element
+    tab = ctk.CTkTabview(APP,
+                         width=APP_WIDTH - 0,
+                         height=APP_HEIGHT - 0,
+                         fg_color=GRAY_90_COLOR,
+                         border_color=LIGHT_GREEN_COLOR,
+                         segmented_button_selected_color=LIGHT_BLUE_COLOR,
+                         segmented_button_selected_hover_color=DARK_BLUE_COLOR, )  # Bordas laterais = 10px
+    tab.pack(padx=0,
+             pady=0)
+
+    # endregion
+
+    # region 🟩 Main Tab Names
+    # Defining names for the main interface tabs
+    tab_1 = 'Home'
+    tab_2 = "Info"
+
+    # Adding tabs to the interface
+    tab.add(tab_1)
+    tab.add(tab_2)
+
+    # Variables representing the tabs
+    first_tab = tab.tab(tab_1)
+    second_tab = tab.tab(tab_2)
+
+    # endregion
+
+    # Define the main tab
+    tab.set(tab_1)
+
+    # endregion
+
+    # region 1️⃣ First Tab (Home)
     
-    # Create the main frame
-    main_frame = ctk.CTkFrame(master=APP)
-    main_frame.pack(pady=20,
-                    padx=20, 
-                    fill="both",
-                    expand=True)
+    # Create the home container
+    home_container_first_tab = ctk.CTkFrame(master=first_tab,
+                                            fg_color=TRANSPARENT_COLOR)
+    home_container_first_tab.pack(fill="both",
+                                  expand=True)
+
+    # region 🟩 Sliders + Button (Container)
 
     # Create a slider for choosing between encryption and decryption
     slider_value = ctk.StringVar()
-    slider = ctk.CTkSwitch(master=main_frame,
+    slider = ctk.CTkSwitch(master=home_container_first_tab,
                            text="Decryption",
                            variable=slider_value,
                            onvalue="on",
@@ -42,26 +82,32 @@ def main():
     slider.pack(pady=10, 
                 padx=10)
 
+    # endregion
+
     # Create an entry field for the encryption key
-    entry_key = ctk.CTkEntry(master=main_frame,
+    entry_key = ctk.CTkEntry(master=home_container_first_tab,
                              placeholder_text="Enter the key here")
     entry_key.pack(pady=5,
                    padx=10,
                    fill=ctk.X)
 
+    # region 🟥 Text Boxes (Container)
+
     # Create a text box for displaying encrypted/decrypted content
-    text_box = ctk.CTkTextbox(master=main_frame,
+    text_box = ctk.CTkTextbox(master=home_container_first_tab,
                               height=100)
     text_box.pack(pady=5,
                   padx=10,
                   fill=ctk.BOTH)
 
-    # region 🟦 "Footer Container"
+    # endregion
+
+    # region 🟦 Footer (Container)
 
     # Container ▶️ "Create" Button
     main_tab_footer_container = ctk.CTkFrame(
-        master=main_frame,
-        fg_color=GRAY_80_COLOR,
+        master=home_container_first_tab,
+        fg_color=TRANSPARENT_COLOR,
         bg_color=TRANSPARENT_COLOR)
     main_tab_footer_container.pack(side=ctk.BOTTOM,
                                    fill=ctk.X,
@@ -73,7 +119,7 @@ def main():
     start_button = ctk.CTkButton(master=main_tab_footer_container,
                                  text="Start",
                                  height=40,
-                                 width=340,
+                                 width=360,
                                  fg_color=LIGHT_BLUE_COLOR,
                                  hover_color=DARK_BLUE_COLOR,
                                  font=HIGHLIGHT_FONT,
@@ -100,6 +146,146 @@ def main():
     copy_button.pack(pady=10,
                      padx=5,
                      side=ctk.RIGHT)
+
+    # endregion
+    
+    # endregion
+
+    # region 2️⃣ Second Tab (Info)
+
+    # region 🟨 Frame of Frames (smaller groups)
+    info_tab_container = ctk.CTkFrame(master=second_tab,
+                                      border_width=1,
+                                      border_color=GRAY_50_COLOR)
+    info_tab_container.pack(side=ctk.TOP,
+                            padx=5,
+                            pady=5,
+                            fill=ctk.X,
+                            expand=True,
+                            anchor="n")
+    # endregion
+
+    # region 🟨 First group
+
+    # FRAME: Desenvolvedor
+
+    info_tab_first_frame_group = ctk.CTkFrame(master=info_tab_container,
+                                              fg_color=TRANSPARENT_COLOR)
+    info_tab_first_frame_group.pack(side=ctk.TOP, fill=ctk.X, expand=True,
+                                    anchor="n", pady=2, padx=2)
+
+    info_tab_first_frame_group_first_info_text = ctk.CTkLabel(
+        master=info_tab_first_frame_group, text="DEVELOPER",
+        text_color=GRAY_COLOR, font=SMALL_FONT)
+    info_tab_first_frame_group_first_info_text.pack(side=ctk.LEFT, fill=ctk.X,
+                                                    anchor='w', expand=False,
+                                                    ipadx=20)
+
+    info_tab_first_frame_group_first_text_value = ctk.CTkLabel(
+        master=info_tab_first_frame_group, text=DEVELOPER_NAME,
+        text_color=LIGHT_BLUE_COLOR,
+        font=HIGHLIGHT_FONT)
+    info_tab_first_frame_group_first_text_value.pack(side=ctk.RIGHT,
+                                                     anchor='w', expand=True,
+                                                     ipadx=20)
+
+    # endregion
+
+    # region 🟨 Second group
+
+    # FRAME: App Name
+
+    info_tab_second_frame_group = ctk.CTkFrame(master=info_tab_container,
+                                               fg_color=TRANSPARENT_COLOR)
+    info_tab_second_frame_group.pack(side=ctk.TOP, fill=ctk.X, expand=True,
+                                     anchor="n", padx=2)
+
+    info_tab_second_frame_group_first_info_text = ctk.CTkLabel(
+        master=info_tab_second_frame_group, text="APP NAME",
+        text_color=GRAY_COLOR,
+        font=SMALL_FONT)
+    info_tab_second_frame_group_first_info_text.pack(side=ctk.LEFT, fill=ctk.X,
+                                                     anchor='w', expand=False,
+                                                     ipadx=20)
+
+    info_tab_second_frame_group_first_info_text_space = ctk.CTkLabel(
+        master=info_tab_second_frame_group, text='   ',
+        text_color=GRAY_COLOR, font=SMALL_FONT)
+    info_tab_second_frame_group_first_info_text_space.pack(side=ctk.LEFT,
+                                                           fill=ctk.X,
+                                                           anchor='w',
+                                                           expand=False)
+
+    info_tab_second_frame_group_first_text_value = ctk.CTkLabel(
+        master=info_tab_second_frame_group, text=APP_NAME,
+        text_color=LIGHT_BLUE_COLOR,
+        font=HIGHLIGHT_FONT)
+    info_tab_second_frame_group_first_text_value.pack(side=ctk.RIGHT,
+                                                      anchor='w', expand=True,
+                                                      ipadx=20)
+
+    # endregion
+
+    # region 🟨 Third group
+
+    # FRAME: Version
+
+    info_tab_third_frame_group = ctk.CTkFrame(master=info_tab_container,
+                                              fg_color=TRANSPARENT_COLOR)
+    info_tab_third_frame_group.pack(side=ctk.TOP, fill=ctk.X, expand=True,
+                                    anchor="n", pady=2, padx=2)
+
+    info_tab_third_frame_group_first_info_text = ctk.CTkLabel(
+        master=info_tab_third_frame_group, text="VERSION",
+        text_color=GRAY_COLOR,
+        font=SMALL_FONT)
+    info_tab_third_frame_group_first_info_text.pack(side=ctk.LEFT, fill=ctk.X,
+                                                    anchor='w', expand=False,
+                                                    ipadx=20)
+
+    info_tab_third_frame_group_first_info_text_space = ctk.CTkLabel(
+        master=info_tab_third_frame_group, text='      ',
+        text_color=GRAY_COLOR, font=SMALL_FONT)
+    info_tab_third_frame_group_first_info_text_space.pack(side=ctk.LEFT,
+                                                          fill=ctk.X,
+                                                          anchor='w',
+                                                          expand=False)
+
+    info_tab_third_frame_group_first_text_value = ctk.CTkLabel(
+        master=info_tab_third_frame_group,
+        text=APP_VERSION,
+        text_color=LIGHT_RED_COLOR,
+        font=HIGHLIGHT_FONT)
+    info_tab_third_frame_group_first_text_value.pack(side=ctk.RIGHT,
+                                                     anchor='w', expand=True,
+                                                     ipadx=20)
+
+    # endregion
+
+    # region 🚫 Optional Spacer
+    info_tab_third_frame_group_end_spacer = ctk.CTkLabel(master=second_tab,
+                                                         text='')
+    info_tab_third_frame_group_end_spacer.pack(expand=True, fill=ctk.Y)
+    # endregion
+
+    # region 🟨 FOOTER (Text + action button)
+    # Info text
+    info_tab_footer_info_text = ctk.CTkLabel(master=second_tab,
+                                             text="Automations, Upgrades or Support?",
+                                             text_color=GRAY_COLOR,
+                                             font=MEDIUM_FONT)
+    info_tab_footer_info_text.pack(side=ctk.TOP, fill=ctk.X, expand=True,
+                                   anchor="s")
+
+    # "Contact" Button
+    info_tab_footer_button = ctk.CTkButton(master=second_tab,
+                                           text="Contact Developer",
+                                           command=open_profile,
+                                           height=40,
+                                           fg_color=LIGHT_BLUE_COLOR,
+                                           hover_color=DARK_BLUE_COLOR)
+    info_tab_footer_button.pack(side=ctk.BOTTOM, fill=ctk.X, padx=5, pady=5)
+    # endregion
 
     # endregion
 
