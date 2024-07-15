b3ae9adf0e49b5029344f649e8072317ed9c8564
GustavoRosas-Dev <python.dev.br@gmail.com>
2024-07-15 02:49:16 -0300

    ⚡ General update:
    - Refactoring and reorganization of global variables:
      - Renaming `slider` and `slider_value` to `first_slider` and `first_slider_value`.
      - Addition of `second_slider`, `second_slider_value`, and `first_button`.
    
    - Enhancements to the main tab interface (Home):
      - Addition of `first_tab_header_container` to organize top components.
      - Introduction of a second slider (`second_slider`) to toggle between text and file selection.
      - Inclusion of `first_button` for file search, initially hidden and shown based on `second_slider` selection.
    
    - Adjustments to component organization and layout:
      - Relocation and behavior changes for sliders (`first_slider` and `second_slider`).
      - Reorganization of containers to improve code structure and readability.
    
    - Update of command functions:
      - Addition of `change_first_slider_name` and `change_second_slider_name` commands for slider manipulation.
      - Inclusion of `process_file` function triggered by `first_button`.
    
    - Other improvements and adjustments:
      - Layout and padding adjustments for components (`entry_key`, `text_box`, `start_button`, `copy_button`).
      - General enhancements to user interface layout and flow.

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ from src.manager import *
 from src.function_manager import *
 
 # Declare global variables
-slider = None
-slider_value = None
+first_slider = None
+second_slider = None
+first_slider_value = None
+second_slider_value = None
+first_button = None
 entry_key = None
 text_box = None
 start_button = None
@@ -21,7 +24,8 @@ def main():
     A function that sets up the main user interface elements for the application.
     """
 
-    global slider, slider_value, entry_key, text_box, start_button
+    global first_slider, second_slider, first_slider_value, second_slider_value, first_button, entry_key, text_box, \
+        start_button
 
     # region 🟢 Tabs
 
@@ -61,43 +65,74 @@ def main():
     # endregion
 
     # region 1️⃣ First Tab (Home)
-    
-    # Create the home container
-    home_container_first_tab = ctk.CTkFrame(master=first_tab,
-                                            fg_color=TRANSPARENT_COLOR)
-    home_container_first_tab.pack(fill="both",
-                                  expand=True)
+
+    # Container ▶️ "Create" Button
+    first_tab_header_container = ctk.CTkFrame(
+        master=first_tab,
+        fg_color=TRANSPARENT_COLOR,
+        bg_color=TRANSPARENT_COLOR)
+    first_tab_header_container.pack(side=ctk.TOP,
+                                    fill=ctk.X,
+                                    expand=False,
+                                    anchor="n",
+                                    padx=5)
 
     # region 🟩 Sliders + Button (Container)
 
     # Create a slider for choosing between encryption and decryption
-    slider_value = ctk.StringVar()
-    slider = ctk.CTkSwitch(master=home_container_first_tab,
-                           text="Decryption",
-                           variable=slider_value,
-                           onvalue="on",
-                           offvalue="off",
-                           progress_color=LIGHT_BLUE_COLOR,
-                           command=lambda: change_switch_name(slider))
-    slider.pack(pady=10, 
-                padx=10)
+    first_slider_value = ctk.StringVar()
+    first_slider = ctk.CTkSwitch(master=first_tab_header_container,
+                                 text="Decryption",
+                                 variable=first_slider_value,
+                                 onvalue="on",
+                                 offvalue="off",
+                                 progress_color=LIGHT_BLUE_COLOR,
+                                 command=lambda: change_first_slider_name(first_slider))
+    first_slider.pack(pady=10,
+                      side=ctk.LEFT)
+
+    # Create a slider for choosing between text and file
+    second_slider_value = ctk.StringVar()
+    second_slider = ctk.CTkSwitch(master=first_tab_header_container,
+                                  text="Text",
+                                  variable=second_slider_value,
+                                  onvalue="on",
+                                  offvalue="off",
+                                  progress_color=LIGHT_BLUE_COLOR,
+                                  command=lambda: change_second_slider_name(second_slider,
+                                                                            first_button,
+                                                                            text_box,
+                                                                            copy_button))
+    second_slider.pack(pady=10,
+                       padx=20,
+                       side=ctk.LEFT)
+
+    first_button = ctk.CTkButton(master=first_tab_header_container,
+                                 text="Search File",
+                                 height=30,
+                                 width=100,
+                                 fg_color=LIGHT_RED_COLOR,
+                                 hover_color=DARK_RED_COLOR,
+                                 command=process_file)
+    first_button.pack(side=ctk.RIGHT)
+    first_button.pack_forget()
 
     # endregion
 
+    # region 🟥 Entry Fields
+    
     # Create an entry field for the encryption key
-    entry_key = ctk.CTkEntry(master=home_container_first_tab,
+    entry_key = ctk.CTkEntry(master=first_tab,
                              placeholder_text="Enter the key here")
     entry_key.pack(pady=5,
-                   padx=10,
+                   padx=5,
                    fill=ctk.X)
 
-    # region 🟥 Text Boxes (Container)
-
     # Create a text box for displaying encrypted/decrypted content
-    text_box = ctk.CTkTextbox(master=home_container_first_tab,
-                              height=100)
+    text_box = ctk.CTkTextbox(master=first_tab,
+                              height=110)
     text_box.pack(pady=5,
-                  padx=10,
+                  padx=5,
                   fill=ctk.BOTH)
 
     # endregion
@@ -105,45 +140,45 @@ def main():
     # region 🟦 Footer (Container)
 
     # Container ▶️ "Create" Button
-    main_tab_footer_container = ctk.CTkFrame(
-        master=home_container_first_tab,
+    first_tab_footer_container = ctk.CTkFrame(
+        master=first_tab,
         fg_color=TRANSPARENT_COLOR,
         bg_color=TRANSPARENT_COLOR)
-    main_tab_footer_container.pack(side=ctk.BOTTOM,
-                                   fill=ctk.X,
-                                   expand=False,
-                                   anchor="n",
-                                   padx=5)
+    first_tab_footer_container.pack(side=ctk.BOTTOM,
+                                    fill=ctk.X,
+                                    expand=False,
+                                    anchor="n")
 
     # Create a button to start the encryption/decryption process
-    start_button = ctk.CTkButton(master=main_tab_footer_container,
+    start_button = ctk.CTkButton(master=first_tab_footer_container,
                                  text="Start",
                                  height=40,
-                                 width=360,
                                  fg_color=LIGHT_BLUE_COLOR,
                                  hover_color=DARK_BLUE_COLOR,
                                  font=HIGHLIGHT_FONT,
                                  command=lambda: process_message(entry_key,
                                                                  text_box,
-                                                                 slider_value,
+                                                                 first_slider_value,
                                                                  pad_key_to_32_bytes,
                                                                  fernet,
                                                                  base64,
                                                                  messagebox))
-    start_button.pack(pady=10,
+    start_button.pack(pady=5,
                       padx=5,
                       side=ctk.LEFT,
-                      fill=ctk.X)
+                      fill=ctk.X,
+                      expand=True)
 
-    # Create a button to copy the content of the text box
-    copy_button = ctk.CTkButton(master=main_tab_footer_container,
+    # Create a button to copy the content of the text boxI
+    copy_button = ctk.CTkButton(master=first_tab_footer_container,
                                 text="🗐",
                                 font=("Material Symbols Outlined", 24),
                                 height=40,
+                                width=40,
                                 fg_color=LIGHT_GREEN_COLOR,
                                 hover_color=DARK_GREEN_COLOR,
                                 command=lambda: copy_content_to_clipboard(APP, text_box))
-    copy_button.pack(pady=10,
+    copy_button.pack(pady=5,
                      padx=5,
                      side=ctk.RIGHT)
 
