4d4c75cb00fc3ed7cbe40f21508ea0f4f022ec26
GustavoRosas-Dev <python.dev.br@gmail.com>
2024-07-15 02:11:01 -0300

    ✨ New feature: Added 'change_second_slider_name' function
    - Implemented functionality to manage the switch between 'Text' and 'File' modes for the second slider

--- src/functions/change_switch_name.py
+++ src/functions/change_switch_name.py
@@ -19,3 +19,21 @@ def change_first_slider_name(first_slider):
         first_slider.configure(text="Decryption")
 
 
+# Function to change the second_slider (Text-File) name
+def change_second_slider_name(second_slider, first_button, text_box, copy_button):
+    """
+    Change the name of the switch between "Encryption" and "Decryption".
+    """
+
+    current_text = second_slider.cget("text")
+    if current_text == "Text":
+        second_slider.configure(text="File")
+        first_button.pack(side="right")
+        text_box.delete("1.0", "end")
+        text_box.configure(state="disabled")
+        copy_button.pack_forget()
+    elif current_text == "File":
+        second_slider.configure(text="Text")
+        first_button.pack_forget()
+        text_box.configure(state="normal")
+        copy_button.pack(side="right", padx=5)
