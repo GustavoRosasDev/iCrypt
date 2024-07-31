9ae13fbb04dad010a2e3519167109b13167ea410
GustavoRosas-Dev <python.dev.br@gmail.com>
2024-07-31 13:44:20 -0300

    ⚡ General update: Update script to include dropdown configuration and customtkinter
    - Added import for customtkinter as ctk
    - Modified change_second_slider_name function to include configuration for first_dropdown
    - Set default values for first_dropdown based on second_slider state

--- src/functions/change_switch_name.py
+++ src/functions/change_switch_name.py
@@ -5,6 +5,8 @@ Developer: Gustavo Rosas
 Profile: https://www.linkedin.com/in/gustavorosas-/
 """
 
+import customtkinter as ctk
+
 
 # Function to change the first_slider (Encryption-Decryption) name
 def change_first_slider_name(first_slider):
@@ -20,7 +22,12 @@ def change_first_slider_name(first_slider):
 
 
 # Function to change the second_slider (Text-File) name
-def change_second_slider_name(second_slider, first_button, text_box, copy_button):
+def change_second_slider_name(second_slider,
+                              first_button,
+                              text_box,
+                              copy_button,
+                              first_dropdown
+                              ):
     """
     Change the name of the switch between "Encryption" and "Decryption".
     """
@@ -29,11 +36,17 @@ def change_second_slider_name(second_slider, first_button, text_box, copy_button
     if current_text == "Text":
         second_slider.configure(text="File")
         first_button.pack(side="right")
+        text_box.configure(state="normal")
         text_box.delete("1.0", "end")
         text_box.configure(state="disabled")
         copy_button.pack_forget()
+        first_dropdown.configure(values=["AES-256"])
+        first_dropdown.set("AES-256")  # Set default value when "File" is selected
     elif current_text == "File":
         second_slider.configure(text="Text")
         first_button.pack_forget()
         text_box.configure(state="normal")
+        text_box.delete("1.0", "end")
         copy_button.pack(side="right", padx=5)
+        first_dropdown.configure(values=["AES-128", "AES-256"])
+        first_dropdown.set("AES-128")  # Set default value when "Text" is selected
