0276d786d48043676d9eb3cd29955aeac1524430
GustavoRosas-Dev <python.dev.br@gmail.com>
2024-07-31 13:53:14 -0300

    🔨 Refactor code: - Removed unused import: cryptography.fernet.Fernet - Replaced platform module import with platform.system() function

--- src/etc/imports.py
+++ src/etc/imports.py
@@ -6,8 +6,6 @@ Profile: https://www.linkedin.com/in/gustavorosas-/
 """
 
 # noinspection PyUnresolvedReferences
-from cryptography.fernet import Fernet as fernet
-# noinspection PyUnresolvedReferences
 from tkinter import messagebox
 # noinspection PyUnresolvedReferences
 import customtkinter as ctk
@@ -16,7 +14,7 @@ from PIL import Image
 # noinspection PyUnresolvedReferences
 import webbrowser
 # noinspection PyUnresolvedReferences
-from sys import platform
+from platform import system
 # noinspection PyUnresolvedReferences
 import base64
 # noinspection PyUnresolvedReferences
